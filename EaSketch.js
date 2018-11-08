ee7c273e813f9ca848ffa768a28e543267a52f7a
Kevin Harris <kcharris77@gmail.com>
2018-11-08 03:00:07 -0600

    Finish after 4-5 hours

---
+++ EaSketch.js
@@ -0,0 +1,31 @@
+let containerDiv = document.querySelector('.grid');
+let num = 16
+function createPix() {
+    for (i = 0; i < num * num; i++) {
+        const newDiv = document.createElement('div');
+        containerDiv.appendChild(newDiv);
+    }
+}
+function pixulate() {
+    const pixs = document.querySelectorAll(".grid div");
+    pixs.forEach((pix) => {
+        pix.addEventListener('mouseover', () => {
+            pix.classList.add('hovered')
+        });
+    })
+}
+createPix()
+pixulate()
+const btn = document.createElement('button');
+document.querySelector('body').insertBefore(btn, document.querySelector('body').firstChild);
+btn.textContent = "Click to reset";
+btn.addEventListener('click', () => {
+    num = prompt("Please input a grid size:  ", "");
+    let gridRe = document.querySelector('.grid');
+    gridRe.setAttribute('style', `grid-template-rows: repeat(` + num + `, 1fr); grid-template-columns: repeat(` + num + `, 1fr);`);
+    while (gridRe.firstChild){
+        gridRe.removeChild(gridRe.firstChild);
+    }
+    createPix()
+    pixulate()
+})
